02e24faa7a0dc94e5697d6117c1fc63cea9011ec
LiskIsBest <Darienmoore2002@gmail.com>
2022-07-25 04:55:30 -0500

    added app file

---
+++ app.py
@@ -0,0 +1,10 @@
+from json import load
+from backend import create_app
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=os.environ.get("PORT"))
