885048c9df8650c8cf9706da34a8170ba79a5913
LiskIsBest <Darienmoore2002@gmail.com>
2022-10-10 01:33:20 -0500

    wip rest api

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,90 +1,145 @@
 from flask import Flask, redirect, render_template, request, session
-# from flask_pymongo import PyMongo
-from pymongo import MongoClient, update_one, find, insert_one
-from .extenstions import osuApi
+from flask_pymongo import PyMongo
+from flask_restful import Resource, Api
+
 import requests
 from requests.sessions import Session
-import time
+
 from threading import Thread,local
 from queue import Queue
 
-def create_app(config_object="backend.config"):
+import datetime
+import os
+import json
+
+from dotenv import load_dotenv
+
+from ossapi import *
+
+load_dotenv()
+
+CLIENT_ID: int = os.environ.get("CLIENT_ID")
+CLIENT_SECRET: str = os.environ.get("CLIENT_SECRET")
+REDIRECT_URL: str = os.environ.get("REDIRECT_URL")
+
+osuApi = OssapiV2(CLIENT_ID, CLIENT_SECRET,REDIRECT_URL)
+
+def create_app(config_object="backend.config") -> None:
+
+    """ Flask app initialization """
     app = Flask(__name__)
     app.config.from_object(config_object)
+    api = Api(app=app)
 
+    mongo = PyMongo()
+
+    """ Commafy jinja filter: adds commas to numbers """
     @app.template_filter('commafy')
-    def commafy(value):
+    def commafy(value)->str:
         if value == 9_999_999_999:
             return "No rank"
         else:
             return format(int(value), ',d')
-    mongo = PyMongo(app)
 
-    import datetime
-
-    def makeUser(api: object, username: str) -> dict:
+    """ Returns User data in dictionary """
+    def makeUser(username: str) -> dict:
+        osuApi = OssapiV2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URL)
         if username == "":
             username = "None"
         else:
             try:
-                api.user(username).username
+                osuApi.user(username).username
             except ValueError:
                 username = "None"
-        user_id = api.user(username).id
-        getRank = lambda mode, username: 9_999_999_999 if (api.user(username,mode=mode).rankHistory == None) else api.user(username,mode=mode).rankHistory.data[-1]
+        user_id = osuApi.user(username).id
+        getRank = lambda mode, username: 9_999_999_999 if (osuApi.user(username,mode=mode).rankHistory == None) else osuApi.user(username,mode=mode).rankHistory.data[-1]
         return {"_id": user_id, 
-                "username": api.user(user_id).username,
+                "username": osuApi.user(user_id).username,
                 "osu_rank": getRank(username=username,mode="osu"), 
                 "mania_rank": getRank(username=username,mode="mania"),
                 "taiko_rank": getRank(username=username,mode="taiko"),
                 "fruits_rank": getRank(username=username,mode="fruits"),
-                "avatar_url": api.user(username).avatar_url,
-                "last_time_refreshed": datetime.datetime.now().replace(microsecond=0)
+                "avatar_url": osuApi.user(username).avatar_url,
+                "last_time_refreshed": str(datetime.datetime.now().replace(microsecond=0))
                 }
 
-    @app.route("/update/<username>")
-    def updateUser(username):
-        with MongoClient() as mongo:
+    """ Flask_restful: get user data """
+    class UserData(Resource):
+        def get(self,username):
+            mongo.init_app(app)
+            user_database = mongo.db.users
+            request_username: str = "None" if username == "" else username
+            try:
+                user_id = osuApi.user(request_username).id
+            except:
+                # id for None user
+                user_id = 1516945
+
+            if user_database.find_one({"_id":user_id}) != None:
+                user_data =user_database.find_one({"_id":user_id})
+            else:
+                user_data = makeUser(username=osuApi.user(user_id).username)
+                user_database.insert_one(user_data)
+
+            # data_to_return = json.dumps(user_data, indent=4)
+
+            return user_data
+
+        def put(self,username):
+            mongo.init_app(app)
             user_database = mongo.db.users
             request_username = "None" if username == "" else username
             user_id = osuApi.user(request_username).id
-            user_database.update_one({"_id":user_id},{ "$set" :makeUser(api=osuApi,username=user_id)})
+            user_database.update_one({"_id":user_id},{ "$set" :makeUser(username=user_id)})
+
+    # """ Flask_restful: update user data """
+    # class UserUpdate(Resource):
+    #     def get(self,username):
+    #         mongo.init_app(app)
+    #         user_database = mongo.db.users
+    #         request_username = "None" if username == "" else username
+    #         user_id = osuApi.user(request_username).id
+    #         user_database.update_one({"_id":user_id},{ "$set" :makeUser(username=user_id)})
+
+    # @app.route("/update/string:<username>")
+    # def updateUser(username: str)->None:
+    #     # with MongoClient() as mongo:
+    #     user_database = mongo.db.users
+    #     request_username = "None" if username == "" else username
+    #     user_id = osuApi.user(request_username).id
+    #     user_database.update_one({"_id":user_id},{ "$set" :makeUser(api=osuApi,username=user_id)})
     
-    @app.route("/retrieve/<mode>/<username>",methods=["GET"])
-    def getUser(mode: str, username):
-        user_database = mongo.db.users
+    # @app.route("/retrieve/<string:username>",methods=["GET","POST"])
+    # def getUser(username: str)->dict:
+    #     user_database = mongo.db.users
         
-        request_mode = "mania" if mode == None else mode
-        request_username = "None" if username == "" else username
-        user_id: int = 0
-        user_data: dict = {}
+    #     request_username: str = "None" if username == "" else username
+    #     user_id: int = 0
+    #     user_data: dict = {}
         
-        try:
-            user_id = osuApi.user(request_username).id
-        except:
-            # id for None user
-            user_id = 1516945
+    #     try:
+    #         user_id = osuApi.user(request_username).id
+    #     except:
+    #         # id for None user
+    #         user_id = 1516945
+    #     if user_database.find_one({"_id":user_id}) != None:
+    #         user_data =user_database.find_one({"_id":user_id})
+    #     else:
+    #         user_data = makeUser(api=osuApi,username=osuApi.user(user_id).username)
+    #         user_database.insert_one(user_data)
+        
+    #     return user_data
+    #     # return {"_id": 1, 
+    #     #         "username": "Bob",
+    #     #         "osu_rank": 12, 
+    #     #         "mania_rank": 12,
+    #     #         "taiko_rank": 12,
+    #     #         "fruits_rank": 12,
+    #     #         "avatar_url": "https://www.copahost.com/blog/wp-content/uploads/2019/07/imgsize2.png",
+    #     #         "last_time_refreshed": datetime.datetime.now().replace(microsecond=0)
+    #     #         }
     
-        if user_database.find_one({"_id":user_id}) != None:
-            user_data =user_database.find({"_id":user_id})
-        else:
-            user_data = makeUser(api=osuApi,username=osuApi.user(user_id).username)
-            user_database.insert_one(user_data)
-        
-        data_requested: dict = {}
-            
-        if request_mode != "all":
-            data_requested = {
-                "_id":user_data["_id"],
-                f"{request_mode}_rank":user_data[f"{request_mode}_rank"],
-                "avatar_url": user_data["avatar_url"],
-                "last_time_refreshed": user_data["last_time_refreshed"]
-            }
-        else:
-            data_requested = user_data
-        
-        return data_requested
-    
+    """ route for home page """
     @app.route('/', methods=["GET"])
     def users():
         session["userlist"] = []
@@ -96,9 +151,11 @@ def create_app(config_object="backend.config"):
         session["request_mode"] = request_mode
         session["request_string"] = request_string
 
+        # request_params=[{"mode":request_mode, "username": username} for username in username_list]
+
         userList =[]
 
-        url_list = [f"{request.url_root}retrieve/{request_mode}/{username}" for username in username_list]
+        url_list = [f"{request.url_root}users/{username}" for username in username_list]
         q = Queue(maxsize=0)            #Use a queue to store all URLs
         for url in url_list:
             q.put(url)
@@ -126,7 +183,7 @@ def create_app(config_object="backend.config"):
                 t_worker.start()
             q.join()
 
-        get_all_data(url_list)
+        get_all_data(urls=url_list)
         
         def keyRank(user):
             return user[f"{request_mode}_rank"]
@@ -142,8 +199,11 @@ def create_app(config_object="backend.config"):
     
         return render_template("index.html")
 
+    """ route for update """
     @app.route('/update', methods=["GET"])
     def update():
+        request_mode = session["request_mode"]
+        request_string = session["request_string"]
         user_list = session["userlist"]
         username_list = [user["username"] for user in user_list]
 
@@ -156,7 +216,7 @@ def create_app(config_object="backend.config"):
         # except PyMongo.BulkWriteError as bwe:
         #     print(bwe.details)
 
-        url_list = [f"{request.url_root}update/{username}" for username in username_list]
+        url_list = [f"{request.url_root}users/{username}" for username in username_list]
         q = Queue(maxsize=0)            #Use a queue to store all URLs
         for url in url_list:
             q.put(url)
@@ -171,8 +231,7 @@ def create_app(config_object="backend.config"):
             session = get_session()
             while True:
                 url = q.get()
-                # with session.get(url) as response: pass
-                response = session.get(url)
+                with session.put(url) as response: pass
                 q.task_done()          # tell the queue, this url downloading work is done
 
         def update_all_users(urls) -> None:
@@ -192,5 +251,8 @@ def create_app(config_object="backend.config"):
         session.modified = True
 
         return redirect(f"/?mode={request_mode}&usernames={request_string}")
+
+    api.add_resource(UserData,"/users/<string:username>")
+    # api.add_resource(UserUpdate,"/update/<string:username>")
 
     return app
