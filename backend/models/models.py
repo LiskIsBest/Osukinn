0ab410d38c5b631580a314ff94db87d885170cf5
LiskIsBest <Darienmoore2002@gmail.com>
2022-12-03 07:08:50 -0600

    working on pydantic model for user data

---
+++ backend/models/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field, validator
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+class user(BaseModel):
+    id: int = Field(alias="_id")
+    username: str
+
+
