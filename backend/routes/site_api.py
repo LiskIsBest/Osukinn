678e4955eab29c7131ca14a9ad068d89c7edca54
LiskIsBest <Darienmoore2002@gmail.com>
2022-12-04 05:19:10 -0600

    reverted to non pydantic

--- backend/routes/site_api.py
+++ backend/routes/site_api.py
@@ -1,9 +1,5 @@
 from flask import Blueprint, request
 from pymongo import MongoClient
-from bson import json_util
-import json
-
-from ..models.models import user_data_class
 
 from ossapi import *
 
@@ -21,14 +17,11 @@ REDIRECT_URL: str = os.environ.get("REDIRECT_URL")
 api = Blueprint("api",__name__)
 
 """ Returns User data in dictionary """
-def makeUser(username: str) -> user_data_class:
+def makeUser(username: str) -> dict:
 
     # osu api connection
     osuApi = OssapiV2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URL)
-
-    # pull user id
-    user_id = osuApi.user(username).id
-
+    
     # Check if username is valid. if not set name to "None"
     match username:
         case "":
@@ -38,23 +31,23 @@ def makeUser(username: str) -> user_data_class:
                 osuApi.user(username).username
             except ValueError:
                 username = "None"
+    
+    # pull user id
+    user_id = osuApi.user(username).id
 
     # function to pull global rank for specified gamemode. 9_999_999_999 used as No rank found value
     getRank = lambda mode, username: 9_999_999_999 if (osuApi.user(username,mode=mode).rankHistory == None) else osuApi.user(username,mode=mode).rankHistory.data[-1]
-
-    return user_data_class(
-            user_id = user_id,
-            username = osuApi.user(user_id).username,
-            osu_rank = getRank(username=username,mode="osu"), 
-            mania_rank = getRank(username=username,mode="mania"),
-            taiko_rank = getRank(username=username,mode="taiko"),
-            fruits_rank = getRank(username=username,mode="fruits"),
-            avatar_url = osuApi.user(username).avatar_url,
-            last_time_refreshed = str(datetime.datetime.now().replace(microsecond=0))
-            ).dict()
-
-def parse_json(data):
-    return json.loads(json_util.dumps(data))
+    
+    # dictionary matching MongoDB document layout
+    return {"_id": user_id, 
+            "username": osuApi.user(user_id).username,
+            "osu_rank": getRank(username=username,mode="osu"), 
+            "mania_rank": getRank(username=username,mode="mania"),
+            "taiko_rank": getRank(username=username,mode="taiko"),
+            "fruits_rank": getRank(username=username,mode="fruits"),
+            "avatar_url": osuApi.user(username).avatar_url,
+            "last_time_refreshed": str(datetime.datetime.now().replace(microsecond=0))
+            }
 
 @api.route("<string:username>",methods=["GET","PUT"])
 def data(username) -> dict:
@@ -76,14 +69,14 @@ def data(username) -> dict:
 
     match request.method:
         case "GET":
-            if user_database.find_one({"user_id":user_id}) != None:
-                user_data = user_database.find_one({"user_id":user_id})
+            if user_database.find_one({"_id":user_id}) != None:
+                user_data = user_database.find_one({"_id":user_id})
             else:
                 user_data = makeUser(username=user_id)
                 user_database.insert_one(user_data)
 
-            return parse_json(user_data)
+            return user_data
 
         case "PUT":
-            user_database.update_one({"user_id":user_id},{ "$set" :makeUser(username=user_id)})
+            user_database.update_one({"_id":user_id},{ "$set" :makeUser(username=user_id)})
             return '', 204
