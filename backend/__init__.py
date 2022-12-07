f1159f3a58e365f5a13856151635b71a061cc1c7
Darien Moore <89608814+LiskIsBest@users.noreply.github.com>
2022-12-07 12:42:58 -0600

    Removed commafy filter from __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,14 +7,5 @@ app = Flask(__name__)
 app.config.from_object("backend.config")
 CORS(app)
 
-""" commafy jinja filter: adds commas to numbers """
-@app.template_filter('commafy')
-def commafy(value)->str:
-    if value == 9_999_999_999:
-        return "No rank"
-    else:
-        # ex: 123456 -> 123,456
-        return format(int(value), ',d')
-
 app.register_blueprint(main.main, url_prefix="/")
 app.register_blueprint(site_api.api,url_prefix="/users/")
