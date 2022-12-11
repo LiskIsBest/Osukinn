b677e394e1b22164498a9c7dd4331798adef6590
LiskIsBest <Darienmoore2002@gmail.com>
2022-12-11 17:59:15 -0600

    added top plays for each mode in makeUser function

--- backend/routes/site_api.py
+++ backend/routes/site_api.py
@@ -38,7 +38,7 @@ def makeUser(username: str) -> dict:
     user = osuApi.users(user_ids=[osuApi.user(username).id])[0]
 
     # function to pull global rank for specified gamemode. 9_999_999_999 used as No rank found value
-    def getRank(user, mode):
+    def get_rank(user, mode):
         rankStat = eval(f"user.statistics_rulesets.{mode}")
         if rankStat != None:
             if rankStat.global_rank == None:
@@ -46,16 +46,38 @@ def makeUser(username: str) -> dict:
             return rankStat.global_rank
         return 9_999_999_999
 
+    def get_song_data(user_id, mode):
+        songs = osuApi.user_scores(user_id=user_id,type_="best", limit=5,mode=mode)
+        return f"top_five_{mode}",[
+            {
+                "place" : index+1,
+                "song_id" : score.best_id,
+                "accuracy" : score.accuracy,
+                "mods" : str(score.mods),
+                "score" : score.score,
+                "max_combo" : score.max_combo,
+                "unweighted_pp" : score.pp,
+                "weighted_pp" : score.weight.pp,
+                "weight" : score.weight.percentage,
+                "mode" : mode
+            }
+            for index, score in enumerate(songs)
+        ]
+
     # dictionary matching MongoDB document layout
     return {"_id": user.id,
             "publicId": user.id,
             "username": user.username,
-            "osu_rank": getRank(user=user,mode="osu"), 
-            "mania_rank": getRank(user=user,mode="mania"),
-            "taiko_rank": getRank(user=user,mode="taiko"),
-            "fruits_rank": getRank(user=user,mode="fruits"),
+            "osu_rank": get_rank(user=user,mode="osu"), 
+            "mania_rank": get_rank(user=user,mode="mania"),
+            "taiko_rank": get_rank(user=user,mode="taiko"),
+            "fruits_rank": get_rank(user=user,mode="fruits"),
             "avatar_url": user.avatar_url,
-            "last_time_refreshed": str(datetime.datetime.now().replace(microsecond=0))
+            "last_time_refreshed": str(datetime.datetime.now().replace(microsecond=0)),
+            (osu_songs := get_song_data(user_id=user.id,mode="osu"))[0]:osu_songs[1],
+            (mania_songs := get_song_data(user_id=user.id,mode="mania"))[0]:mania_songs[1],
+            (taiko_songs := get_song_data(user_id=user.id,mode="taiko"))[0]:taiko_songs[1],
+            (fruits_songs := get_song_data(user_id=user.id,mode="fruits"))[0]:fruits_songs[1],
             }
 
 @api.route("<string:username>",methods=["GET","PUT"])
