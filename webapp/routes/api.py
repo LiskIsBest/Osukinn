8aaf0d3435d1999d8f21931a9395c271ff898719
Darien Moore <Darienmoore2002@gmail.com>
2023-01-14 02:24:31 -0600

    working api for getting and updated documents

--- webapp/routes/api.py
+++ webapp/routes/api.py
@@ -4,6 +4,7 @@ import json
 from fastapi import APIRouter, status
 
 from ..extensions import NewOsuApiConnection, NewMongoConnection
+from ..models.models import User, Song, Modes
 
 router = APIRouter(prefix="/users")
 
@@ -33,21 +34,21 @@ def makeUser(username):
     # pull user data
     user = osuApi.users(user_ids=[osuApi.user(username).id])[0]
 
-    # dictionary matching MongoDB document layout
-    return {"_id": user.id,
-            "publicId": user.id,
-            "username": user.username,
-            "osu_rank": getRank(user=user, mode="osu"), 
-            "mania_rank": getRank(user=user, mode="mania"),
-            "taiko_rank": getRank(user=user, mode="taiko"),
-            "fruits_rank": getRank(user=user, mode="fruits"),
-            "avatar_url": user.avatar_url,
-            "last_time_refreshed": datetime.now().replace(microsecond=0),
-            (osu_songs := getSongData(api=osuApi, user_id=user.id, mode="osu"))[0] : osu_songs[1],
-            (mania_songs := getSongData(api=osuApi, user_id=user.id, mode="mania"))[0] : mania_songs[1],
-            (taiko_songs := getSongData(api=osuApi, user_id=user.id, mode="taiko"))[0] : taiko_songs[1],
-            (fruits_songs := getSongData(api=osuApi, user_id=user.id, mode="fruits"))[0] : fruits_songs[1],
-            }
+    data = User(
+            public_id = user.id,
+            username = user.username,
+            osu_rank = getRank(user=user, mode=Modes.OSU.value),
+            mania_rank = getRank(user=user, mode=Modes.MANIA.value),
+            taiko_rank = getRank(user=user, mode=Modes.TAIKO.value),
+            fruits_rank = getRank(user=user, mode=Modes.MANIA.value),
+            avatar_url = user.avatar_url,
+            osu_songs = getSongData(api=osuApi, user_id=user.id, mode=Modes.OSU.value),
+            mania_songs = getSongData(api=osuApi, user_id=user.id, mode=Modes.MANIA.value),
+            taiko_songs = getSongData(api=osuApi, user_id=user.id, mode=Modes.TAIKO.value),
+            fruits_songs = getSongData(api=osuApi, user_id=user.id, mode=Modes.CTB.value)
+            )
+    
+    return data
 
 # function to pull global rank for specified gamemode. 9_999_999_999 used as "No rank" found value
 def getRank(user, mode):
@@ -61,19 +62,31 @@ def getRank(user, mode):
 # returns list of top 5 plays for given mode
 def getSongData(api, user_id, mode):
     songs = api.user_scores(user_id=user_id, type_="best", limit=5, mode=mode)
-    return f"top_five_{mode}", [
-        {
-            "place" : index+1,
-            "song_id" : score.best_id,
-            "accuracy" : score.accuracy,
-            "mods" : str(score.mods),
-            "score" : score.score,
-            "max_combo" : score.max_combo,
-            "unweighted_pp" : score.pp,
-            "weighted_pp" : score.weight.pp,
-            "weight" : score.weight.percentage,
-            "mode" : mode
-        }
+    return [
+        # {
+        #     "place" : index+1,
+        #     "song_id" : score.best_id,
+        #     "accuracy" : score.accuracy,
+        #     "mods" : str(score.mods),
+        #     "score" : score.score,
+        #     "max_combo" : score.max_combo,
+        #     "unweighted_pp" : score.pp,
+        #     "weighted_pp" : score.weight.pp,
+        #     "weight" : score.weight.percentage,
+        #     "mode" : mode
+        # }
+        Song(
+            place=index+1,
+            song_id=score.best_id,
+            accuracy=score.accuracy,
+            mods=str(score.mods),
+            score=score.score,
+            max_combo=score.max_combo,
+            unweighted_pp=score.pp,
+            weighted_pp=score.weight.pp,
+            weight=score.weight.percentage,
+            mode=mode
+        ).dict()
         for index, score in enumerate(songs)
     ]
 
@@ -82,8 +95,8 @@ def userJsonSerializer(value):
     if isinstance(value, (date, datetime)):
         return value.isoformat(sep=" ")
 
-@router.get("/{username}", status_code=status.HTTP_200_OK)
-async def get_data(username: str = "None"):
+@router.get("/{username}", response_model=None, status_code=status.HTTP_200_OK)
+async def get_data(username:str="None") -> any:
     osuApi = NewOsuApiConnection()
 
     mongo = NewMongoConnection()
@@ -99,17 +112,18 @@ async def get_data(username: str = "None"):
         # id for "None" user
         user_id = 1516945
 
-    if (data := userCollection.find_one({"_id" : user_id})) != None:
-        user_data = data
+    if (user_doc:= userCollection.find_one({"public_id" : user_id})) != None:
+        user_data = user_doc
+        mongo.close()
+        return json.dumps(user_data, default=userJsonSerializer, indent=3)
     else:
         user_data = makeUser(username=user_id)
-        userCollection.insert_one(user_data)
+        userCollection.insert_one(user_data.dict(by_alias=True))
+        mongo.close()
+        return user_data.json(by_alias=True)
 
-    mongo.close()
-    return json.dumps(user_data, default=userJsonSerializer, indent=3)
-
-@router.put("/{username}", status_code=status.HTTP_200_OK)
-async def update(username: str = "None"):
+@router.put("/{username}", response_model=None ,status_code=status.HTTP_200_OK)
+async def update(username:str="None") -> any:
     osuApi = NewOsuApiConnection()
 
     mongo = NewMongoConnection()
@@ -125,7 +139,7 @@ async def update(username: str = "None"):
         # id for "None" user
         user_id = 1516945
 
-    userCollection.update_one({"_id" : user_id},{"$set" : makeUser(username=user_id)})
+    userCollection.update_one({"_id" : user_id},{"$set" : makeUser(username=user_id).dict(by_alias=True)})
 
     mongo.close()
     return {f"{username}":"Updated"}
