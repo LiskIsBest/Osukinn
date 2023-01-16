f5a0becbb8fe9cb778aa2d20ceff158886f45a16
Darien Moore <Darienmoore2002@gmail.com>
2023-01-16 05:11:10 -0600

    removed commented junk code

--- frontend/src/components/Refresh.svelte
+++ frontend/src/components/Refresh.svelte
@@ -12,19 +12,6 @@
 	function endpoint(username) {
     return `${location.origin}/users/${username}`;
   }
-	
-  // username_list = username_list.toString();
-	// async function refresh(){
-	// 	username_list.forEach(async (username) => {
-	// 		const response = await fetch(endpoint(username));
-  //     const data = await response.json();
-	// 		userList.update(contents => [...contents, JSON.parse(data)])
-	// 		userList.subscribe(value => {
-	// 			user_data = value
-	// 		})
-	// 	});
-	// }
-
 	async function refresh(){
 		username_list.forEach(async (username) => {
 			let response = await axios.put(endpoint(username))
@@ -35,15 +22,5 @@
 </script>
 
 <div>
-  <!-- <form class={className} method="get" action="/update">
-    <button>refresh users</button>
-    <input type="hidden" name="mode" id="mode" value={mode} />
-    <input
-      type="hidden"
-      name="usernames"
-      id="usernames"
-      value={username_list}
-    />
-  </form> -->
 	<button class={className} on:click={refresh}>Refresh Users</button>
 </div>
