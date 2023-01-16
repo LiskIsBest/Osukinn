d1adc9043d4f547270cf59ca9f293d0e2675f088
Darien Moore <Darienmoore2002@gmail.com>
2023-01-16 01:21:36 -0600

    partially working refresh button

--- frontend/src/components/Refresh.svelte
+++ frontend/src/components/Refresh.svelte
@@ -1,18 +1,49 @@
 <script>
-  export let username_list;
-  export let mode;
+	import { userList } from "../stores";
+	import axios from "axios";
+  import User from "./User.svelte";
 
-  username_list = username_list.toString();
+  let className = "";
+  export { className as class };
+
+  export let username_list;
+  // export let mode;
+
+	function endpoint(username) {
+    return `${location.origin}/users/${username}`;
+  }
+	
+  // username_list = username_list.toString();
+	// async function refresh(){
+	// 	username_list.forEach(async (username) => {
+	// 		const response = await fetch(endpoint(username));
+  //     const data = await response.json();
+	// 		userList.update(contents => [...contents, JSON.parse(data)])
+	// 		userList.subscribe(value => {
+	// 			user_data = value
+	// 		})
+	// 	});
+	// }
+
+	async function refresh(){
+		username_list.forEach(async (username) => {
+			let response = await axios.put(endpoint(username))
+			console.log(JSON.parse(response.data))
+		});
+		userList.update(contents => [...contents])
+	}
 </script>
 
 <div>
-  <form
-    class="col-12 text-center"
-    method="get"
-    action="/update"
-  >
+  <!-- <form class={className} method="get" action="/update">
     <button>refresh users</button>
-    <input type="hidden" name="mode" id="mode" value={mode}/>
-    <input type="hidden" name="usernames" id="usernames" value={username_list}/>
-  </form>
+    <input type="hidden" name="mode" id="mode" value={mode} />
+    <input
+      type="hidden"
+      name="usernames"
+      id="usernames"
+      value={username_list}
+    />
+  </form> -->
+	<button class={className} on:click={refresh}>Refresh Users</button>
 </div>
