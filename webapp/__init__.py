4fa48f9e1faae5ba4fd92d5fbb61ddd4cb291365
Darien Moore <Darienmoore2002@gmail.com>
2023-01-16 00:20:46 -0600

    added cors, fixed router mount
    added CORS just in case

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,17 +2,25 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import webapp.routes.api_router as api_router
 
 script_dir = os.path.dirname(__file__)
 frontendAbsolutePath = os.path.join(script_dir, "../frontend/dist")
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(api_router.router, prefix="/users")
 
 app.mount("", StaticFiles(directory=frontendAbsolutePath, html=True), name="frontend")
 
-app.include_router(api_router.router, prefix="/users")
 
 @app.get('/')
-async def frontend():
+def frontend():
    return RedirectResponse(url='frontend')
