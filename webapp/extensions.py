19f74cb31b4c86cfa4986790eb395739c149605f
Darien Moore <Darienmoore2002@gmail.com>
2023-02-13 04:21:29 -0600

    created async connection methods

--- webapp/extensions.py
+++ webapp/extensions.py
@@ -1,13 +1,30 @@
 import os
-from enum import Enum
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from losuapi import OsuApi
+from motor.motor_asyncio import AsyncIOMotorClient
+from losuapi import OsuApi, AsyncOsuApi
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URI")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+        
+def NewAsyncMotorConnection() -> AsyncIOMotorClient:
+    try:
+        return AsyncIOMotorClient(MONGO_URL)
+    except ConnectionError:
+        if MONGO_URL == None:
+            raise Exception("MONGO_URI not set. Check environment variables.")
+        else:
+            raise Exception(f"Unable to connect to MongoDB Client.")
+
+def NewAsyncOsuApiConnection() -> AsyncOsuApi:
+    return AsyncOsuApi(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+        
+
+# unused
+def NewOsuApiConnection() -> OsuApi:
+    return OsuApi(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
 def NewMongoConnection() -> MongoClient:
     try:
@@ -17,6 +34,3 @@ def NewMongoConnection() -> MongoClient:
             raise Exception("MONGO_URI not set. Check environment variables.")
         else:
             raise Exception(f"Unable to connect to MongoDB Client.")
-        
-def NewOsuApiConnection() -> OsuApi:
-    return OsuApi(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
